6064692c8ceef1a067c77f8e87574f76498a1571
Paulo Netto <pauloestrella1994@gmail.com>
2020-08-24 18:21:21 -0300

    Fixing datetime fields problems

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ import datetime
 
 
 def timestamp_now():
-    return str(datetime.datetime.now())[:-7]
+    return datetime.datetime.now()
 
 class Sellers(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
